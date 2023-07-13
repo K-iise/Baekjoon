c01f90f95e699c5c85eff12bc7a775d6e6961b32
K-iise <91732817+K-iise@users.noreply.github.com>
2023-07-14 00:08:16 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "#  # #### #### #  #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#  # #### #### #  #";
+
+}
+
+
+
