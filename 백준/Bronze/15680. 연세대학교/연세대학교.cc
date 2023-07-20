914a2111577df401a2fd9952683c58f1df1a1119
K-iise <91732817+K-iise@users.noreply.github.com>
2023-07-20 23:53:50 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+
+	cin >> N;
+
+	if (N == 0)
+		cout << "YONSEI";
+	else if (N == 1)
+		cout << "Leading the Way to the Future";
+}
+
+
+
