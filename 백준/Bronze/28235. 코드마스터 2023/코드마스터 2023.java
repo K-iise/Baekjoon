c436be8ea736d6b51076edb82cb41c6cce72af51
K-iise <91732817+K-iise@users.noreply.github.com>
2024-01-21 23:43:02 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 120 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		
+		String str = br.readLine();
+		
+		
+		if(str.equals("SONGDO"))
+			bw.write("HIGHSCHOOL");
+		else if(str.equals("CODE"))
+			bw.write("MASTER");
+		else if(str.equals("2023"))
+			bw.write("0611");
+		else if(str.equals("ALGORITHM"))
+			bw.write("CONTEST");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
