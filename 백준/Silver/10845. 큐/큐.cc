36417a1963130b319a6f5a8b8f74eb1a023db22e
K-iise <91732817+K-iise@users.noreply.github.com>
2025-04-03 13:29:20 +0900

    [Silver IV] Title: 큐, Time: 208 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int N;
+    cin >> N;
+    queue<int> que;
+
+    while(N--){
+        string str;
+        cin >> str;
+
+        if(str == "push"){
+            int tmp;
+            cin >> tmp;
+            que.push(tmp);
+        }
+        else if(str == "pop"){
+            if(que.empty()) cout << "-1\n";
+            else{
+                cout << que.front() << "\n";
+                que.pop();
+            }
+        }
+        else if(str == "size"){
+            cout << que.size() << "\n";
+        }
+        else if(str == "empty"){
+            if(que.empty()) cout << "1\n";
+            else cout << "0\n";
+        }
+        else if(str == "front"){
+            if(que.empty()) cout << "-1\n";
+            else cout << que.front() << "\n";
+        }
+        else if(str == "back"){
+            if(que.empty()) cout << "-1\n";
+            else cout << que.back() << "\n";
+        }
+    }
+}
