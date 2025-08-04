757101e2c4f74b0de6df2fe61294b49abc202bbe
K-iise <91732817+K-iise@users.noreply.github.com>
2025-08-04 17:41:53 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        long dp[][] = new long[101][10];
+        long MOD = 1000000000L;
+
+
+        int N = Integer.parseInt(st.nextToken());
+        long result = 0;
+        for (int i = 1; i < 10; i++){
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++){
+
+            for (int j = 0; j < 10; j++){
+
+                if(j == 0){
+                    dp[i][j] = dp[i-1][1] % MOD;
+                }
+                else if(j == 9){
+                    dp[i][j] = dp[i-1][8] % MOD;
+                }
+                else
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % MOD;
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            result = (result + dp[N][i]) % MOD;
+        }
+
+        bw.write(result + "");
+        bw.flush();
+
+    }
+}
