12453448aac8224634a8d5612bed1756248b9a26
Yun Gi Kim <kimyungi1004@naver.com>
2026-03-02 15:22:21 +0900

    [Silver V] Title: 삽입 정렬을 해볼까, Time: 1284 ms, Memory: 177188 KB -BaekjoonHub

---
+++ 백준/Silver/33846. 삽입 정렬을 해볼까/삽입 정렬을 해볼까.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int []arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, 0, t);
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
