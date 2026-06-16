df1466d00a045a71e8da7262db17949f32022442
Yun Gi Kim <kimyungi1004@naver.com>
2026-06-16 12:30:08 +0900

    [level 2] Title: [1차] 캐시, Time: 24.45 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,40 +1,37 @@
 import java.util.*;
 
-class Solution {
-    public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
-        int LRU = 0;
-        
-        cities = Arrays.stream(cities).map(String::toLowerCase).toArray(String[]::new);
-        
-        Map<String, Integer> cache = new HashMap<>();
-        
-        for(String city : cities){
-            if(cache.size() < cacheSize && !cache.containsKey(city)){
-                cache.put(city, LRU++);
-                answer += 5; // cache miss
+ class Solution {
+        public int solution(int cacheSize, String[] cities) {
+            int answer = 0;
+            List<String> hash = new LinkedList<>();
+
+            if (cacheSize == 0) {
+                answer = cities.length * 5;
+                return answer;
             }
-            else if(cache.containsKey(city)){
-                answer += 1; // cache hit;
-                cache.replace(city, LRU++);
-            }
-            else if(cache.size() == cacheSize) {
-                
-                if(cache.size() == 0){
-                    answer += 5; // cache miss
+
+            for (String city : cities) {
+                String lowerCity = city.toLowerCase(); // 소문자로 변경
+
+                // 맵이 있으면 히트 시킨다.
+                if (hash.contains(lowerCity)) {
+                    answer += 1; // hit
+                    hash.remove(lowerCity);
+                    hash.add(lowerCity);
                 }
-                else{
-                ArrayList<Integer> alist = new ArrayList<>(cache.values());
-                Collections.sort(alist);
-                
-                int l = alist.get(0); // value 얻기.
-                cache.values().remove(l);
-                cache.put(city, LRU++);
-                answer += 5; // cache miss
+
+                else {
+                    // 캐시 사이즈가 다 찬 경우 LRU에 따라서 제거.
+                    if (hash.size() == cacheSize) {
+                        // 최소값이 제일 적게 사용한거니까?
+                        hash.removeFirst();
+                    }
+                    answer += 5;
+                    hash.add(lowerCity);
                 }
+
+
             }
+            return answer;
         }
-        
-        return answer;
     }
-}
