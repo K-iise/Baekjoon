5b94c9267fc4573e5b9929483c4a69f333ab99a9
Yun Gi Kim <kimyungi1008@gmail.com>
2026-07-28 14:45:18 +0900

    [level 2] Title: 무인도 여행, Time: 7.15 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,55 +1,67 @@
 import java.util.*;
 
 class Solution {
-    boolean check[][];
-    int n, m;
-    List<Integer> ans = new ArrayList<>();
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static boolean [][]visit;
     
     public int[] solution(String[] maps) {
-        n = maps.length; // 세로
-        m = maps[0].length(); // 가로
         
-        check = new boolean[n][m];
+        List<Integer> answer = new ArrayList<>();
+        int n = maps.length;
+        int m = maps[0].length();
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (check[i][j] || maps[i].charAt(j) == 'X') continue;
-                DFS(j, i, maps);  // DFS 호출
-            }
-        }
+        visit = new boolean[n][m];
         
-        if (ans.isEmpty()) return new int[]{-1};
-        return ans.stream().mapToInt(Integer::intValue).sorted().toArray();
-    }
-    
-    public void DFS(int x, int y, String maps[]) {
-        Queue<int[]> que = new LinkedList<>();
-        
-        que.add(new int[]{x, y});
-        check[y][x] = true;
-        
-        int ny[] = {-1, 1, 0, 0};
-        int nx[] = {0, 0, -1, 1};
-        int sum = 0;
-
-        while (!que.isEmpty()) {
-            int[] curr = que.poll();
-            int cx = curr[0];
-            int cy = curr[1];
-            sum += maps[cy].charAt(cx) - '0';  // 현재 좌표 값 더하기
-            
-            for (int k = 0; k < 4; k++) {
-                int ry = cy + ny[k];
-                int rx = cx + nx[k];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 
-                if (ry >= 0 && ry < n && rx >= 0 && rx < m) {  // 유효한 범위인지 확인
-                    if (!check[ry][rx] && maps[ry].charAt(rx) != 'X') {
-                        que.add(new int[]{rx, ry});
-                        check[ry][rx] = true;
+                if(maps[i].charAt(j) != 'X' && !visit[i][j]) {               
+                    int sum = 0;
+                    Queue<int[]> q = new ArrayDeque<>();
+                    q.offer(new int[]{i, j});
+                    visit[i][j] = true;
+                    
+                    while(!q.isEmpty()) {
+                        var t = q.poll();
+                        
+                        int x = t[0];
+                        int y = t[1];
+                        
+                        sum += maps[x].charAt(y) - '0';
+                        
+                        for(int k = 0; k < 4; k++) {
+                            
+                            int px = x + dx[k];
+                            int py = y + dy[k];
+                            
+                           if (px < 0 || px >= n || py < 0 || py >= m) {
+                                continue;
+                            }
+
+                            if (maps[px].charAt(py) == 'X' || visit[px][py]) {
+                                continue;
+                            }
+                            
+                            visit[px][py] = true;
+                            q.offer(new int[]{px, py});
+                        }
                     }
+                
+                    answer.add(sum);
                 }
             }
         }
-        ans.add(sum);
+        
+        if(answer.isEmpty()) {
+            return new int[]{-1};
+        }
+        
+        Collections.sort(answer);
+        
+        return answer.stream()
+                .sorted()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
