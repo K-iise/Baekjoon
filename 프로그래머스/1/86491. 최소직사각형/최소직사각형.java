4fe91ba1993cbaf36fffd4e86af98da4c7d22e11
Yun Gi Kim <kimyungi1008@gmail.com>
2026-09-10 17:41:35 +0900

    [level 1] Title: 최소직사각형, Time: 0.61 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int row = 0;
+        int col = 0;
+        
+        for(int i = 0; i < sizes.length; i++) {
+            int w = Math.max(sizes[i][0], sizes[i][1]);
+            int h = Math.min(sizes[i][0], sizes[i][1]);
+            
+            row = Math.max(row, w);
+            col = Math.max(col, h);
+        }
+        
+        
+        answer = row * col;
+        return answer;
+    }
+}
